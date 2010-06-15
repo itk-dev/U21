4daee50812f78762925442d1a44159ddf74f48c9
Jesper Kristensen <cableman@linuxdev.dk>
2010-06-15 15:34:54 +0200

    Fixed taxonomy link box count (now panels_slug aware)

--- sites/all/modules/u21_content/u21_content_panels_content_taxonomy_links.tpl.php
+++ sites/all/modules/u21_content/u21_content_panels_content_taxonomy_links.tpl.php
@@ -1,9 +1,11 @@
 <h2><?php print $title ?></h2>
 <ul>
   <?php foreach ($terms as $term) { ?>
+      <?php if ($term['count']) { ?>
         <li>
             <?php print $term['link'] ?>
             <span class="node-count"><?php print $term['count'] ?></span>
         </li>
+      <?php } ?>
   <?php } ?>
 </ul>
