5c0d463344dad5f163d9b7451acbca640b2886c7
Jesper Kristensen <cableman@linuxdev.dk>
2010-07-05 14:56:42 +0200

    Taxonomy links under news fixed

--- sites/all/modules/u21_content/u21_content_panels_content_taxonomy_links.tpl.php
+++ sites/all/modules/u21_content/u21_content_panels_content_taxonomy_links.tpl.php
@@ -1,9 +1,13 @@
 <ul>
-  <?php foreach ($terms as $term) { ?>
+  <li>
+    <?php print $terms['all']['link'] ?>
+  </li>
+  <?php foreach ($terms as $key => $term) { ?>
+      <?php if ($key == 'all') { continue; }?>
       <?php if ($term['count']) { ?>
         <li>
             <?php print $term['link'] ?>
-            <span class="node-count"><?php print $term['count'] ?></span>
+            <span class="node-count">(<?php print $term['count'] ?>)</span>
         </li>
       <?php } ?>
   <?php } ?>
