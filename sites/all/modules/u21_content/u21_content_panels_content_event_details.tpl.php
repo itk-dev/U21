349810d44ad53d5b6c6e86667cf09267882a72e5
Jesper Kristensen <cableman@linuxdev.dk>
2010-08-30 18:51:58 +0200

    Remove empty timestamp on events plugin

--- sites/all/modules/u21_content/u21_content_panels_content_event_details.tpl.php
+++ sites/all/modules/u21_content/u21_content_panels_content_event_details.tpl.php
@@ -4,8 +4,10 @@
     <td><?php print $date ?></td>
   </tr>
   <tr>
+    <?php if ($time_start != '00:00') { ?>
     <td class="label"><?php print t('Time') ?>:</td>
     <td><?php print $time_start ?> - <?php print $time_end ?></td>
+    <?php } ?>
   </tr>
   <tr>
     <td class="label"><?php print t('Price') ?>:</td>
